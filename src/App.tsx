2261d171b89be7692c2145d32b7b4a067634d392
Remo <remo@kali.com>
2021-05-06 03:52:48 -0400

    pass navigation data-pass same into comp as props-set props to any

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 import 'reset-css';
 import NavbarScroller from './components/NavbarScroller'
 
-class App extends Component {
-  render () {
+const navigation = {
+  brand: { name: "NavbarScroller", to: "/"},
+  links: [
+    { name: "Home", to: "/home" },
+    { name: "Launches", to: "/launches" },
+    { name: "Succeeded", to: "/succeeded" },
+    { name: "Failed", to: "/failed" },
+    { name: "Upcoming", to: "/upcoming"},
+    { name: "Support", to: "/support"}
+  ]
+}
+
+export default class App extends Component {
+  // the public is a typescript feature
+  public render () {
+    // Destructured object for cleaner code
+    const { brand, links } = navigation;
     return (
       <div className="App">
-        <NavbarScroller />
+        <NavbarScroller brand={brand} links={links}/>
       </div>
     )
   }
 }
 
-export default App;
+
--- src/components/NavbarScroller.tsx
+++ src/components/NavbarScroller.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 
-const NavbarScroller = () => {
+const NavbarScroller = (props: any) => {
+    // this completely defeats the purpose of using typescript
     return (
         <div>
-
+            <p>NavbarScroller</p>
         </div>
     )
 }
